0014a7ec109cb046923d8bd0cf62eeca89d02b63
shekhovtsovsergey <98221024+shekhovtsovsergey@users.noreply.github.com>
2022-10-02 19:05:03 +0300

    Init

---
+++ src/main/java/netty/FileServer.java
@@ -0,0 +1,46 @@
+package netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FileServer {
+
+    public static void main(String[] args) {
+        EventLoopGroup auth = new NioEventLoopGroup(1);
+        EventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(auth, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<io.netty.channel.socket.SocketChannel>() {
+                        @Override
+                        protected void initChannel(io.netty.channel.socket.SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(
+                                    new BufToString(),
+                                    new StringToBuf(),
+                                    new EchoHandler()
+                            );
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.bind(8189).sync();
+            log.debug("Server is ready on port: " + 8189);
+            channelFuture.channel().closeFuture().sync(); // block
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            auth.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+}
